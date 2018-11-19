4879dce28971f523388f0ea2ae486f5dc9606337
bornfree <harsha.xg@gmail.com>
2018-11-19 19:08:32 +0530

    Remove unised imports

--- api.py
+++ api.py
@@ -2,9 +2,6 @@ from sanic import Sanic
 from sanic.response import json
 from work import load_assets, shuffle_assets, generate_data
 import os
-import time
-from threading import Thread
-from queue import Queue
 
 app = Sanic()
 
@@ -20,4 +17,4 @@ if __name__ == '__main__':
     load_assets()
     shuffle_assets()
 
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, workers=4)
